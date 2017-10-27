55d7599dbb2875c6a31baf3db87041fe7acb2c84
Olga <helgonaft@gmail.com>
2017-10-28 01:50:42 +0300

    contact form

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    console.log('ready');
     $('.nav li a').click(function(event){
 
         $('html, body').animate({
@@ -13,17 +12,18 @@ $(document).ready(function(){
         var target_id = $(this).attr('data-id');
 
         if($(this).hasClass('is-active')){
-            $('.industry').removeClass('is-active');
-            $('.details').slideUp();
-            $('.industry').find('span').removeClass('glyphicon-triangle-top').addClass( 'glyphicon-triangle-bottom');
+            closeDetails();
         }
         else{
-            $('.industry').removeClass('is-active');
-            $('.details').slideUp();
-            $('.industry').find('span').removeClass('glyphicon-triangle-top').addClass( 'glyphicon-triangle-bottom');
+            closeDetails();
             $(target_id).slideDown();
             $(this).addClass('is-active');
             $(this).find('span').removeClass('glyphicon-triangle-bottom').addClass( 'glyphicon-triangle-top');
+        }
+        function closeDetails(){
+            $('.industry').removeClass('is-active');
+            $('.details').slideUp();
+            $('.industry').find('span').removeClass('glyphicon-triangle-top').addClass( 'glyphicon-triangle-bottom');
         }
     });
     
