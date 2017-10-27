917c9ff00306dcc3d3ea03e6b0b03ebd9d86a805
Olga <helgonaft@gmail.com>
2017-10-27 18:31:33 +0300

    changed colors, main page,finished industries section

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    console.log('ready');
+    $('.nav li a').click(function(event){
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+
+        event.preventDefault();
+    });
+
+    $(".industry").click(function(){
+        var target_id = $(this).attr('data-id');
+
+        if($(this).hasClass('is-active')){
+            $('.industry').removeClass('is-active');
+            $('.details').slideUp();
+            $('.industry').find('span').removeClass('glyphicon-triangle-top').addClass( 'glyphicon-triangle-bottom');
+        }
+        else{
+            $('.industry').removeClass('is-active');
+            $('.details').slideUp();
+            $(target_id).slideDown();
+            $(this).addClass('is-active');
+            $(this).find('span').removeClass('glyphicon-triangle-bottom').addClass( 'glyphicon-triangle-top');
+        }
+    });
+    
+});
